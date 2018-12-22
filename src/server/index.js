40b4c236202e90c6ca28b1636e1059ba96fb55d1
More <2495875335@qq.com>
2018-12-22 15:44:58 +0800

    server promise must into promise.all

--- src/server/index.js
+++ src/server/index.js
@@ -14,9 +14,17 @@ app.get('*', function(req, res) {
   const promises = [];
   matchedRoutes.forEach(item => {
     if (item.route.loadData) {
-      promises.push(item.route.loadData(store))
+      const promise = new Promise((resolve, reject) => {
+        item.route.loadData(store).then(resolve).catch(resolve)
+      })
+      promises.push(promise)
+      // promises.push(item.route.loadData(store))
     }
   })
+  // 一个页面要加载 A,B,C,D四个组件，这四个组件都需要从服务端加载数据
+  // 为了无论如何都走Promise.all方法，我们需要将promise再封装一层
+  // 无论单个接口成功与否，都要往到all里 去render.
+  // TODO: 每个接口加个promise.race(, timer) 设置时间。
 
   Promise.all(promises).then(() => {
     res.send(render(req, store, routes))
